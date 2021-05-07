d9734516cde94f3cd6534a617caaad9b4549fab7
Lujain Al-shammari <lujain.alshammari@learnet.se>
2021-05-07 12:44:05 +0200

    Alla modell klasser vi behöver

---
+++ ProjektWebApi/Models/GeoMessage2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektWebApi.Models
+{
+    public class GeoMessage2:GeoMessage
+    {
+        public new MessageOne MessageOne { get; set; }
+    }
+}
