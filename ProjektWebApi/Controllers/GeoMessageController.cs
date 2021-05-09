7d311a5354f0da40bc3f8a298d78a6620cfa85ea
Isabella <isabellapetterson@gmail.com>
2021-05-09 21:09:34 +0200

    register user metod

--- ProjektWebApi/Controllers/GeoMessageController.cs
+++ ProjektWebApi/Controllers/GeoMessageController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using ProjektWebApi.Data;
 using ProjektWebApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProjektWebApi.Controllers
 {
@@ -15,10 +16,12 @@ namespace ProjektWebApi.Controllers
     public class GeoMessageController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<MyUser> _userManager;
 
-        public GeoMessageController(ApplicationDbContext context)
+        public GeoMessageController(ApplicationDbContext context, UserManager<MyUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [Route("GetGeoMessage")]
@@ -88,6 +91,36 @@ namespace ProjektWebApi.Controllers
             catch
             {
                 return BadRequest();
+            }
+        }
+
+        [Route("RegisterUser")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> RegisterUser(string firstName, string lastName, string userName, string password)
+        {
+            if (String.IsNullOrWhiteSpace(userName))
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                MyUser newUser = new MyUser();
+                newUser.FirstName = firstName;
+                newUser.LastName = lastName;
+                newUser.UserName = userName;
+                newUser.PasswordHash = _userManager.PasswordHasher.HashPassword(newUser, password);
+
+                await _userManager.CreateAsync(newUser);
+
+                return Ok();
+
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
             }
         }
     }
